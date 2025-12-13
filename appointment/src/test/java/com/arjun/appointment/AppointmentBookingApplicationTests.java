b23c79a91eff824d944c8ad788eb79891f723965
ajueshchan <nagarjunjeyachandran@gmail.com>
2025-12-13 09:06:29 +0530

    base code changes

---
+++ appointment/src/test/java/com/arjun/appointment/AppointmentBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjun.appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
